3e843c5333365ee8de088dfb29d37e710c5806d4
garrettrh <garrett.harris@gmail.com>
2015-01-22 17:02:46 -0800

    Added title spec.

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe Record, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.build(:record)).to be_valid
   end
-  it "is invalid without a title"
+  it "is invalid without a title" do
+    record = FactoryGirl.build(:record, title: nil)
+    expect(record).to be_invalid
+  end
   it "is invalid without an artist"
   it "is invalid without a genre"
   it "is invalid without a track listing"

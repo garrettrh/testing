626d88e0b5c1bba36f4c911e9622e2d97af5d927
garrettrh <garrett.harris@gmail.com>
2015-01-22 17:05:13 -0800

    Added title validation to model.

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :record do
     title "Straight Outta Compton"
-artist "N.W.A."
-genre "Gangsta Rap"
-track_list "1. Straight Outta Compton 2. Boyz In The Hood"
-liner_notes "Writer: Ice Cube Producer: Dr. Dre"
+    artist "N.W.A."
+    genre "Gangsta Rap"
+    track_list "1. Straight Outta Compton 2. Boyz In The Hood"
+    liner_notes "Writer: Ice Cube Producer: Dr. Dre"
   end
 
 end
